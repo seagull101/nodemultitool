7776196968c2a9fc6cdb8cb9e552b5290404c14a
Bassam Ibrahim <41242242+seagull101@users.noreply.github.com>
2021-08-27 15:34:05 +0000

    + Pbar function

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 //functions
+function pbar(a,b,c){
+	var amount = a/b
+	var result = ""
+	var shadedbars = Math.floor(amount*c) 
+	var clearbars = c-shadedbars
+	for(i = 1;i<c;i++){
+		if(shadedbars >= i){
+			result = result + "|"
+		}else{
+			result = result + "."
+		}
+	}
 
+return result
+}
 //Test Area
 console.log("Quality Testing")
+console.log(pbar(5,7,20))
